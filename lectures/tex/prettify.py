29336d2585e314eb7bed9394f97286d382b54469
Sky-Nik <n.skybytskyi@gmail.com>
2020-10-14 01:03:03 +0300

    heavy updates of part i

--- lectures/tex/prettify.py
+++ lectures/tex/prettify.py
@@ -2,12 +2,26 @@
 import os
 
 for part_name in os.listdir():
-    for file_name in os.listdir(part_name):
-        contents: str = ''
-        with open(file_name, 'r') as input_file:
-            contents = input_file.read()
-        contents.replace(' $', '~$')
-        for previous in ':;,.!?':
-            contents.replace(previous + '~$', previous +' $')
-        with open(file_name, 'w') as output_file:
-            output_file.write(contents)
+    try:
+        os.chdir(part_name)
+
+        for file_name in os.listdir():
+            contents: str = ''
+            with open(file_name, 'r', encoding='utf-8') as input_file:
+                contents = input_file.read()
+
+            # ties (~) before formulas ($)
+            contents = contents.replace(r" $", r'~$')
+            # but bot after special symbols
+            for previous in r":;,.!?[](){}":
+                contents = contents.replace(previous + r"~$", previous + r" $")
+            contents = contents.replace(r"\item~$", r"\item $")
+
+            with open(file_name, 'w', encoding='utf-8') as output_file:
+                output_file.write(contents)
+
+        os.chdir('..')
+    
+    # TODO: add a normal if instead of this workaround
+    except:  # part_name is file not dir
+        pass
